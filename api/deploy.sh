077e3d5205cb875204c2291eafb979195a89d03c
leinue <597055914@qq.com>
2016-09-21 16:42:53 +0800

    update

--- api/deploy.sh
+++ api/deploy.sh
@@ -1,2 +1,3 @@
+docker stop gospel_api && docker rm gospel_api
 docker build -t gospel_api .
-docker run -itd -p 9999:8088 --name="gospel_api" gospel_api
+docker run -itd -p 9999:8088 -w /var/www/api --name="gospel_api" gospel_api
--- dash/deploy.sh
+++ dash/deploy.sh
@@ -1,2 +1,3 @@
+docker stop gospel_dash && docker rm gospel_dash
 docker build -t ivydom/gospel_dash .
-docker run -itd -p 8089:8088 --name="gospel_dash" ivydom/gospel_dash
+docker run -itd -p 8089:8088 -w /var/www/dash --name="gospel_dash" ivydom/gospel_dash
