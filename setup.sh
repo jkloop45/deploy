59bc2cc1c784c48833573796e6accd2eaaeb82c0
leinue <597055914@qq.com>
2016-10-05 17:20:55 +0800

    update

--- setup.sh
+++ setup.sh
@@ -23,6 +23,14 @@ service nginx start
 #install git
 sudo yum install git -y
 
+#install node
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+nvm install v6
+npm install -g cnpm --registry=https://registry.npm.taobao.org
+
 #clone index
 git clone https://github.com/Gospely/index /var/www/gospely/index
 
