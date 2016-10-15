78d883b160920ada5f6fbb6c96b2ed2dedc968b8
SharksevenRo <937257166@qq.com>
2016-10-15 11:25:58 +0800

    update

--- api/deploy.sh
+++ api/deploy.sh
@@ -1,3 +1,3 @@
 docker stop gospel_api && docker rm gospel_api
 docker build -t gospel_api .
-docker run -itd -p 9999:8089 -w /var/www/api --name="gospel_api" gospel_api
+docker run -itd -p 9999:8089 -w /var/www/api --name="gospel_api" --link gospel-redis:redis  gospel_api
