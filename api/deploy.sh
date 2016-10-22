d03066653f49504852f0a2feee388707fcf9055c
leinue <597055914@qq.com>
2016-10-22 10:02:09 +0800

    update

--- api/deploy.sh
+++ api/deploy.sh
@@ -1,5 +1,5 @@
 docker stop gospel_api && docker rm gospel_api
 docker build -t gospel_api .
-docker run -itd -p 9999:8089 -w /var/www/api -v /var/www/ssh:/root/.ssh  --name="gospel_api" --link gospel-redis:redis  gospel_api
+docker run -itd -p 9999:8089 -v /var/www/storage:/var/www/storage -w /var/www/api -v /var/www/ssh:/root/.ssh  --name="gospel_api" --link gospel-redis:redis  gospel_api
 
 cat /var/www/ssh/id_rsa.pub >> /root/.ssh/authorized_keys
