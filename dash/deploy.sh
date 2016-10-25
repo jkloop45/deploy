bd2dd7e31009513b3a1d2f0370538c815921ee81
leinue <597055914@qq.com>
2016-10-25 13:46:25 +0800

    update

--- dash/deploy.sh
+++ dash/deploy.sh
@@ -1,3 +1,3 @@
 docker stop gospel_dash && docker rm gospel_dash
 docker build -t gospel_dash .
-docker run -itd -p 9998:8088 -w /var/www/dash --name="gospel_dash" gospel_dash
+docker run -itd -p 9998:80 -w /usr/share/nginx/html --name="gospel_dash" gospel_dash
