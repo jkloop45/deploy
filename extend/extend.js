9cb8a9d8504df8863836a3351149d943e864ceb5
leinue <597055914@qq.com>
2016-11-11 10:09:33 +0800

    update

---
+++ extend/extend.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+require('shelljs/global');
+var argv = require('yargs')
+  .option('c', {
+    alias : 'container',
+    demand: false,
+    describe: 'container name',
+    type: 'string'
+  })
+  .option('s', {
+    alias : 'size',
+    demand: false,
+    describe: 'device size',
+  })
+  .usage('Usage: extend.js [options]')
+  .example('', 'extend device size')
+  .help('h')
+  .alias('h', 'help')
+  .epilog('Gospely Copyright 2016')
+  .argv;
+
+var containerName = argv.c,
+    size = argv.s;
+
+var runBash = 'docker inspect ' + containerName;
+var result = exec(runBash);
+
+console.log(result);
+
+if(result.code !== 0) {
+   console.error(result);
+}else {
+   console.log('Gosple Container is running, SSH: ' + sshPort, 'Socket: ' + port);
+}
