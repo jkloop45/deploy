4f2bb2d24eb92be9d4690e34498fcfce222ca67d
leinue <597055914@qq.com>
2016-12-28 14:55:20 +0800

    update

--- ide/deploy.sh
+++ ide/deploy.sh
@@ -1,3 +1,3 @@
 docker stop gospel_ide && docker rm gospel_ide
 docker build -t gospel_ide .
-docker run -itd -p 9995:8080 -w /var/www/ide --name="gospel_ide" gospel_ide
+docker run -itd -p 9966:80 -w /var/www/ide --name="gospel_ide" gospel_ide
