ef9138263ad710fa6b83b9cda2e0676369b72602
SharksevenRo <937257166@qq.com>
2016-12-30 12:49:56 +0800

    update

--- shell/mv.sh
+++ shell/mv.sh
@@ -1,1 +1,1 @@
-docker exec $1 bash -c 'cp -r /root/temp/* /root/workspace' &&  docker exec $1 bash -c 'cp -r /root/temp/.* /root/workspace' 
+docker exec $1 bash -c 'mv /root/temp/* /root/workspace' &&  docker exec $1 bash -c 'mv /root/temp/.* /root/workspace' 
