757129a18625bc824e5a4a1d82afcb8fa3835644
SharksevenRo <937257166@qq.com>
2017-01-15 13:22:57 +0800

    update

--- shell/clear.sh
+++ shell/clear.sh
@@ -1,1 +1,1 @@
-bash -c "rm -rf /var/www/storage/codes/$1/$2 && rm -f /etc/nginx/conf.d/$2.gospely.com.conf"
+bash -c "rm -rf /var/www/storage/codes/$1/$2 && rm -f /etc/nginx/conf.d/$2.gospely.com.conf && docker rm -rf $3"
