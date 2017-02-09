48e5e66f9b31bdfe8283eab7106b95580b322d71
SharksevenRo <937257166@qq.com>
2017-02-09 17:23:51 +0800

    update

--- shell/git/addOrigin.sh
+++ shell/git/addOrigin.sh
@@ -1,1 +1,1 @@
-docker exec $1 bash -c "git init && git remote add origin $2"
+docker exec $1 bash -c "git init && git remote add origin $2 && git config user.name $3 --replace-all && git config user.email $4 --replace-all"
--- shell/git/resetOrigin.sh
+++ shell/git/resetOrigin.sh
@@ -1,1 +1,1 @@
-docker exec $1 bash -c "git remote set-url origin $2"
+docker exec $1 bash -c "git remote set-url origin $2 && git config user.name $3 --replace-all && git config user.email $4 --replace-all"
