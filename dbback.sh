406ede4e20e3d84fb683267fde5489991874beb9
SharksevenRo <937257166@qq.com>
2017-03-16 16:56:50 +0800

    update

--- dbback.sh
+++ dbback.sh
@@ -1,1 +1,1 @@
-bash -c " cd /mnt/data && zip -r postgres.zip postgres && git add . && git commit -m 'update' && git push && echo backup: $(date '+%Y%m%d %H:%M:%S') >> /data/log.txt"
+bash -c " cd /mnt/data && zip -r postgres.zip postgres && git add . && git commit -m 'update' && git push && echo backup: $(date '+%Y%m%d %H:%M:%S') >> /mnt/data/log.txt"
