ea6bf6f38d7a2ddab883c5219c7891c25a04c80d
SharksevenRo <937257166@qq.com>
2017-03-16 16:47:21 +0800

    update

--- dbback.sh
+++ dbback.sh
@@ -1,7 +1,1 @@
-zip -r postgres.zip postgres
-
-git add *
-
-git commit -m 'update'
-
-git push
+bash -c " cd /mnt/data && zip -r postgres.zip postgres && git add * && git commit -m 'update' && git push"
