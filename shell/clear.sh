b694e21b02614e912a3b9b306a9c59dae94d314f
Sharkseven <937257166@qq.com>
2017-04-10 10:32:23 +0800

    update

--- shell/clear.sh
+++ shell/clear.sh
@@ -1,1 +1,1 @@
-bash -c "rm -rf /mnt/var/www/storage/codes/$1/$2 && rm -f /mnt/etc/nginx/conf.d/$1/$2 && rm -f /mnt/etc/nginx/conf.d/$2 && docker rm -f $3"
+bash -c "rm -rf /mnt/var/www/storage/codes/$1/$2 || rm -f /mnt/etc/nginx/conf.d/$1/$2 || rm -f /mnt/etc/nginx/conf.d/$2 && docker rm -f $3"
