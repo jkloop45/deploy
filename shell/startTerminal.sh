2a2a9a687803d64b69b8105a814d66a03b5f8402
SharksevenRo <937257166@qq.com>
2017-04-15 13:34:06 +0800

    update

--- shell/startTerminal.sh
+++ shell/startTerminal.sh
@@ -1,1 +1,1 @@
-docker exec $1 bash -c ". /root/.nvm/nvm.sh && cd /root/.gospely/.socket/demo && kill -9 $(netstat -tlnp | grep 3000 |awk '{print $7}' | awk -F '/' '{print $1}') && node app.js"
+docker exec $1 bash -c ". /root/.nvm/nvm.sh && cd /root/.gospely/.socket/demo && node app.js"
